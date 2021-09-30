760337c64d75deeedf5f50da570decba0ce88b12
Denis Korotevich <volk.denis98@gmail.com>
2021-09-30 23:35:17 +0300

    feat(lab3): add solution

--- lab/float_vector.py
+++ lab/float_vector.py
@@ -1,3 +1,34 @@
+def generate_array(length):
+    vector_temp = []
+    if length > 0:
+        print('Введите элементы вектора:')
+        for i in range(1, length + 1):
+            try:
+                element: float = float(input(f'A[{i}] = '))
+                vector_temp.append(element)
+            except ValueError:
+                print('Ошибка: Вектор может содержать только вещественные числа!')
+                break
+        return vector_temp
+    else:
+        print('Ошибка: Размерность массива не может быть меньше 1!')
+        return vector_temp
+
+
+def get_count_min(vector):
+    min_el: float = vector[0]
+    count: int = 0
+
+    for i in vector:
+        if min_el > i:
+            min_el = i
+
+    for i in vector:
+        if min_el == i:
+            count += 1
+    return min_el, count
+
+
 vector_a = []
 n: int = 0
 
@@ -6,22 +37,11 @@ try:
 except ValueError:
     print('Ошибка: Размерность вектора не является целым числом!')
 
-if n > 0:
-    print('Введите элементы вектора:')
-    for i in range(1, n + 1):
-        try:
-            element: float = float(input(f'A[{i}] = '))
-            vector_a.append(element)
-        except ValueError:
-            print('Ошибка: Вектор может содержать только вещественные числа!')
-            break
+vector_a = generate_array(n)
 
-    print('Вектора: ')
-    print(f'A={vector_a}')
+print('Вектора: ')
+print(f'A={vector_a}')
 
-    min_element: float = min(vector_a)
-    countMin: int = vector_a.count(min_element)
+result = get_count_min(vector_a)
 
-    print(f'Минимальное число ({min_element}) встречается в вещественном векторе А: {countMin} ')
-else:
-    print('Ошибка: Размерность массива не может быть меньше 1!')
+print(f'Минимальное число ({result[0]}) встречается в вещественном векторе А: {result[1]} ')
