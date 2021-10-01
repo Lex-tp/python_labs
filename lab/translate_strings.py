faf8d155d6ed2d3758ab3815dcdbf9a2fc0015be
Denis Korotkevich <volk.denis98@gmail.com>
2021-10-01 16:23:47 +0300

    docs: add README.md

--- lab/translate_strings.py
+++ lab/translate_strings.py
@@ -8,12 +8,13 @@ try:
     with open(FILE_INPUT, encoding='utf-8') as file_input:
         for line in file_input:
             words_list = line.split('-')
-            word = words_list.pop(0).strip()
-            for key in ''.join(words_list).split(','):
-                key = key.strip()
-                words = str(words_dict.get(key, False))
-                unique_words = f'{words}, ' if words != 'False' else ''
-                words_dict[key] = f'{unique_words}{word}'
+            if len(words_list) > 1:
+                word = words_list.pop(0).strip()
+                for key in ''.join(words_list).split(','):
+                    key = key.strip()
+                    words = str(words_dict.get(key, False))
+                    unique_words = f'{words}, ' if words != 'False' else ''
+                    words_dict[key] = f'{unique_words}{word}'
     with open(FILE_OUTPUT, 'w', encoding='utf-8') as file_output:
         file_output.write(f'Количество английских слов: {len(words_dict)}\n\n')
         for key in sorted(words_dict):
